788152a8f8f3cf3f08b6306dc82613c4894d0230
sentrie <122269172+sen-trie@users.noreply.github.com>
2025-07-11 12:32:19 +0800

    Updates

---
+++ _app/immutable/chunks/BG8sZpQx.js
@@ -0,0 +1,4 @@
+const n=`CAKE
+123
+123
+`;export{n as default};
---
+++ _app/immutable/nodes/0.CFHaiBoS.js
@@ -0,0 +1,1 @@
+import"../chunks/NZTpNUN0.js";import{v as n,w as a,x as p}from"../chunks/DXY1vrIM.js";import{s}from"../chunks/CU4w_5X8.js";const i=!0,u=Object.freeze(Object.defineProperty({__proto__:null,prerender:i},Symbol.toStringTag,{value:"Module"}));function d(o,r){var e=n(),t=a(e);s(t,()=>r.children),p(o,e)}export{d as component,u as universal};
