b5d477cb64753ebed89f31945cd4e4277b5517cd
sentrie <122269172+sen-trie@users.noreply.github.com>
2025-07-11 11:45:40 +0800

    Structure: Add hud elements

---
+++ src/lib/components/Overlay.svelte
@@ -0,0 +1,48 @@
+<script>
+  import Pane from "./overlay/Pane.svelte";
+  import Audio from "./overlay/Audio.svelte";
+
+  let pageStarted = $state(false);
+  let audioEl = $state(null);
+
+  $effect(() => {
+    if (pageStarted) {
+      audioEl.playBGM;
+    }
+  });
+</script>
+
+<div class="abs overlay-screen">
+  <Audio bind:this={audioEl} />
+  <Pane />
+</div>
+
+{#if !pageStarted}
+  <div class="abs intro-screen">
+    <button
+      onclick={() => {
+        pageStarted = true;
+      }}>START</button
+    >
+  </div>
+{/if}
+
+<style>
+  .abs {
+    position: absolute;
+    z-index: 10;
+    width: 100%;
+    height: 100%;
+    pointer-events: none;
+  }
+
+  .abs > * {
+    pointer-events: all;
+  }
+
+  .overlay-screen {
+  }
+
+  .intro-screen {
+  }
+</style>
---
+++ src/lib/components/overlay/Pane.svelte
@@ -0,0 +1,32 @@
+<script>
+  import { changeFloor } from "$lib/stores/sceneControls";
+
+  let change = () => {};
+  changeFloor.subscribe((fn) => {
+    change = fn;
+  });
+</script>
+
+<div>
+  <button
+    class="bg-amber-50 cursor-pointer"
+    onclick={() => {
+      change(0);
+    }}>Show All</button
+  >
+  <button
+    class="bg-amber-50 cursor-pointer"
+    onclick={() => {
+      change(1);
+    }}>Show Top</button
+  >
+  <button
+    class="bg-amber-50 cursor-pointer"
+    onclick={() => {
+      change(2);
+    }}>Show Bottom</button
+  >
+</div>
+
+<style>
+</style>
