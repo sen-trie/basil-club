01f6b1152230087595bd488dd287b0a37bf5ce32
sentrie <122269172+sen-trie@users.noreply.github.com>
2025-07-11 12:16:38 +0800

    Deploy: Test build

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,22 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+// import { importAssets } from 'svelte-preprocess-import-assets';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
-	}
+    kit: {
+        adapter: adapter({
+            fallback: '404.html'
+        }),
+        paths: {
+            base: process.env.NODE_ENV === 'production' ? '/basil-club' : ''
+        },
+        prerender: {
+            handleHttpError: 'ignore'
+        }
+    },
+    relative: true,
+    preprocess: [vitePreprocess()]
 };
 
 export default config;
