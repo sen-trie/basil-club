c712fa1deb8a73c801ab552fe7c1ad407b72c063
ayaya-pc <122269172+sen-trie@users.noreply.github.com>
2025-07-21 23:36:07 +0800

    Deploy: Add adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,11 @@
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 // import { importAssets } from 'svelte-preprocess-import-assets';
+import adapter from "@sveltejs/adapter-vercel";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-    // adapter: adapter({
-    //   fallback: "404.html",
-    // }),
+    adapter: adapter({}),
     paths: {
       base: process.env.NODE_ENV === "production" ? "/basil-club" : "",
     },
