ec1485dd468bf45de0488ec73768f9aaa50a15dd
ayaya-pc <122269172+sen-trie@users.noreply.github.com>
2025-07-22 09:08:41 +0800

    Deploy: Switch to auto

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,9 @@
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 // import { importAssets } from 'svelte-preprocess-import-assets';
-import adapter from "@sveltejs/adapter-vercel";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-    adapter: adapter({}),
     paths: {
       base: process.env.NODE_ENV === "production" ? "/basil-club" : "",
     },
