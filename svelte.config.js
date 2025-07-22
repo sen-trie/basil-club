f7f3bd426f9580fecbec6d8cc42e30fd8ae1429d
ayaya-pc <122269172+sen-trie@users.noreply.github.com>
2025-07-22 09:36:27 +0800

    Deploy: Readd adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,11 @@
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 // import { importAssets } from 'svelte-preprocess-import-assets';
+import adapter from "@sveltejs/adapter-vercel";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-    paths: {
-      base: process.env.NODE_ENV === "production" ? "" : "",
-    },
+    adapter: adapter(),
     prerender: {
       handleHttpError: "ignore",
     },
